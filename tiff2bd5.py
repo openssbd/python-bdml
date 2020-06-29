60aed40af0c3ef0532a89af8719c5894622d15f0
kkyoda <kkyoda@riken.jp>
2020-06-29 16:20:26 +0900

    Add files via upload

--- tiff2bd5.py
+++ tiff2bd5.py
@@ -17,8 +17,9 @@ class Data(IsDescription):
 	oID	= StringCol(8, dflt="", pos=0)
 	t	= Int32Col(dflt=0, pos=1)
 	entity	= StringCol(8, dflt="", pos=2)
-	x	= UInt16Col(dflt=0, pos=3)
-	y	= UInt16Col(dflt=0, pos=4)
+	sID	= Int32Col(dflt=0, pos=3)
+	x	= UInt16Col(dflt=0, pos=4)
+	y	= UInt16Col(dflt=0, pos=5)
 
 # preprocessing
 img = cv2.imread(img_path)
@@ -33,7 +34,10 @@ contours = [np.squeeze(cnt, axis=1) for cnt in contours]
 # output to bd5 file
 h5file = open_file(hdf5_out, mode="w", title="bd5 file")
 group = h5file.create_group('/', 'data', 'data group')
-table = h5file.create_table('/data', '0', Data, "dataset")
+series = h5file.create_group('/data', '0', 'time index 0')
+object = h5file.create_group('/data/0', 'object', 'object0')
+
+table = h5file.create_table('/data/0/object', '0', Data, "dataset")
 
 data = table.row
 
@@ -43,21 +47,20 @@ for i, cnt in enumerate(contours):
 		data['oID']	= '%06d' % (i)
 		data['t']	= 0
 		data['entity']	= 'line'
+		data['sID'] = i
 		data['x']	= j[0]
 		data['y']	= j[1]
 		data.append()
 	# for closed line
-	data['oID']	= '%06d' % (i)
+	data['oID']	= ' %06d' % (i)
 	data['t']	= 0
 	data['entity']	= 'line'
+	data['sID']	= i
 	data['x']	= cnt[0, 0]
 	data['y']	= cnt[0, 1]
 	data.append()
 	
 
 table.flush()
-
-# how to read a dataset (test code)
-table2 = h5file.get_node('/data', '0').read()
 
 h5file.close()
